aafc7e65813effbb57edff22e40ba9a51c032760
Yohannes Feleke <yfeleke@dev.swf-backstage-dev.com>
2023-05-02 10:03:53 -0400

    Upgrade r-base-core and r-doc-html

--- install_R.sh
+++ install_R.sh
@@ -6,8 +6,8 @@ if [[ $arch == x86_64* ]]; then
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
 	add-apt-repository 'deb http://cloud.r-project.org/bin/linux/debian bullseye-cran40/'
 	apt-get update && apt-get upgrade
-	apt-get -y install r-base-core=4.2.3-1~bullseyecran.0 --allow-downgrades linux-libc-dev- gcc-10-
-	apt-get -y install r-doc-html=4.2.3-1~bullseyecran.0 --allow-downgrades
+	apt-get -y install r-base-core=4.3.0-1~bullseyecran.0 --allow-downgrades linux-libc-dev- gcc-10-
+	apt-get -y install r-doc-html=4.3.0-1~bullseyecran.0 --allow-downgrades
 elif [[ $arch == arm* ]] || [[ $arch = aarch64 ]]; then
     echo "ARM Architecture"
     echo "deb http://http.debian.net/debian sid main" > /etc/apt/sources.list.d/debian-unstable.list
