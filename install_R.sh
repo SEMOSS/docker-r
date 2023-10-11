ba94443350e3ac84dbcc36628e4c8e086c4c67e5
kunal0137 <kunal0137@gmail.com>
2023-10-11 14:06:40 -0400

    Update install_R.sh
    keeping glibc for cuda

--- install_R.sh
+++ install_R.sh
@@ -6,13 +6,13 @@ if [[ $arch == x86_64* ]]; then
 	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key '51716619E084DAB9'
 	add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/'
 	apt-get update
-	apt-get -y install r-base-core=4.2.3-1.2204.0 --allow-downgrades linux-libc-dev- gcc-10-
+	apt-get -y install r-base-core=4.2.3-1.2204.0 --allow-downgrades
 	apt-get -y install r-doc-html=4.2.3-1.2204.0 --allow-downgrades
 elif  [[ $arch == arm* ]] || [[ $arch = aarch64 ]]; then
     echo "ARM Architecture"
     echo "deb http://http.debian.net/debian sid main" > /etc/apt/sources.list.d/debian-unstable.list
     apt-get update
-    apt-get -y install r-base-core=4.2.2-1+b1 --allow-downgrades linux-libc-dev- gcc-10-
+    apt-get -y install r-base-core=4.2.2-1+b1 --allow-downgrades
     apt-get -y install r-doc-html=4.2.2-1+b1 --allow-downgrades
     rm /etc/apt/sources.list.d/debian-unstable.list
     apt-get update
